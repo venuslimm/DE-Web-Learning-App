ff3e09a481067dd3159dc65d8d77972e9cc77fdf
venuslimm <limjiayivenus@gmail.com>
2024-09-13 18:42:02 +0800

    Add llm service

---
+++ llm/src/app.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
+
+// Interacts with openai
+@Injectable()
+export class AppService {
+  private openai: OpenAI;
+
+  constructor() {
+    this.openai = new OpenAI({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+  }
+
+  async generateChatbotResponse(prompt: string): Promise<string> {
+    try {
+      // const response = await this.openai.chat.completions.create({
+      //   model: 'gpt-4o-mini',
+      //   messages: [
+      //     {
+      //       role: 'system',
+      //       content:
+      //         'You are an expert in data domains, specifically data engineering.',
+      //     },
+      //     { role: 'user', content: prompt },
+      //   ],
+      //   // max_tokens: 100, // TODO: Change # of max tokens in response
+      //   temperature: 0.5, // Control the randomness to avoid verbose answers
+      //   top_p: 0.8, // Limits the diversity of the response
+      // });
+
+      // if (!response.choices[0].message.content) {
+      //   throw new Error('Could not generate a response');
+      // }
+
+      // return response.choices[0].message.content;
+      return prompt;
+    } catch (error) {
+      console.error('Error with OpenAI API:', error);
+      throw new Error('Could not generate a response');
+    }
+  }
+}
---
+++ llm/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { env } from '../load-env';
+
+// TODO: settle env var
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    // Allow requests from this origin
+    origin: `http://${env.host}:${env.webPort}`,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  await app.listen(env.thisPort);
+  console.log(`Server is running on port ${env.thisPort}`);
+}
+bootstrap();

2ff6234caaf5e61c926c8c3957e0d99066d92633
venuslimm <limjiayivenus@gmail.com>
2024-09-29 15:04:24 +0800

    Update header

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 
 const Header = () => {
   return (
-      <header className="d-flex justify-content-between align-items-center my-2 border-b">
-        <Link href="/">
-          <h1 className="display-10">To insert app logo here</h1>
+      // <header className="flex justify-content-between align-items-center my-2 border-b">
+      <header className="flex flex-row items-center my-2 border-b">
+        <Link href="/" className='mb-1'>
+          {/* Credits:
+            https://static-00.iconduck.com/assets.00/computer-icon-1024x1024-q34qlzrf.png
+            https://static.vecteezy.com/system/resources/previews/035/193/466/original/data-pipeline-icon-line-illustration-vector.jpg
+          */}
+          <Image src="/resources/logo.png" alt="App Logo" width={64} height={64} className="display-10" />
         </Link>
-        <nav>
+        <h1 className="display-10 ml-5">Learn DE</h1>
+        {/* <nav>
           <ul className="nav">
             <li className="nav-item">
               <a className="nav-link" href="#">About</a>
@@ -16,7 +23,7 @@ const Header = () => {
               <a className="nav-link" href="#">Contact</a>
             </li>
           </ul>
-        </nav>
+        </nav> */}
       </header>
   )
 }
